aacf6128f73f196bfb8651c26eabbb0981639faf
Robert Russo <rrusso@lsu.edu>
2012-06-14 11:15:05 -0500

    Integrating feature request

---
+++ local/feature_request/form.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once $CFG->libdir . '/formslib.php';
+
+class feature_request_form extends moodleform {
+    function definition() {
+        global $USER;
+
+        $_s = function($key, $a = null) {
+            return get_string($key, 'local_feature_request', $a);
+        };
+
+        $m =& $this->_form;
+
+        $params = array('size' => 60);
+        $m->addElement('text', 'email_subject', $_s('email_subject'), $params);
+        $m->setDefault('email_subject', $_s('default_subject'));
+
+        $params = array('rows' => 10, 'cols' => 69);
+        $m->addElement('textarea', 'email_body', $_s('email_body'), $params);
+
+        $disclaimer = get_config('local_feature_request', 'disclaimer');
+
+        if ($disclaimer) {
+            $m->addElement('static', 'disclaimer', $_s('disclaimer'), $disclaimer);
+        }
+
+        $buttons = array(
+            $m->createElement('submit', 'submit', $_s('send')),
+            $m->createElement('cancel')
+        );
+
+        $m->addGroup($buttons, 'buttons', '&nbsp;', array(' '), false);
+    }
+}
---
+++ local/feature_request/lang/en/local_feature_request.php
@@ -0,0 +1,29 @@
+<?php
+
+$string['pluginname'] = 'Feature Request';
+$string['default_subject'] = 'Moodle Feature Request';
+
+$string['send'] = 'Send';
+
+$string['email'] = 'Outbound Email';
+$string['email_help'] = 'This email will receive the feature request submissions';
+
+$string['body_prepend'] = 'Body prepend';
+$string['body_prepend_help'] = 'This information will be prepend to every feature request submission, in the email body.';
+
+$string['email_subject'] = 'Title';
+$string['email_body'] = 'Description';
+
+$string['disclaimer'] = 'Disclaimer';
+$string['disclaimer_help'] = 'If filled in, this disclaimer will be displayed on the feature form.';
+
+$string['success'] = 'Feature request successfully sent';
+$string['failure'] = 'Feature request could not be sent, please press send again or contact the Moodle Administrator.';
+
+$string['default_disclaimer'] = '<p>Items submitted via this form may be tracked by visiting the MDAC web site (<a href = "http://itsweb.lsu.edu/MDAC/index.html">http://itsweb.lsu.edu/MDAC/index.html</a>).</p>
+
+<p>Submissions will be posted along with the name of the submitter.</p>
+
+<p>Should there be any questions or need for further clarification regarding the submission, you may be contacted by a Moodle administrator.</p>
+
+<p>Thank you for your ideas and suggestions.</p>';
