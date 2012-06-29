654c276e49f8c6a27ddaecf3291ed0f64c126615
Robert Russo <rrusso@lsu.edu>
2012-06-29 11:00:57 -0500

    Add user name preference for faculty

--- blocks/cps/events/ues_people_lib.php
+++ blocks/cps/events/ues_people_lib.php
@@ -3,11 +3,16 @@
 require_once $CFG->dirroot . '/blocks/ues_people/lib.php';
 
 class cps_people_element extends ues_people_element_output {
+    private function span_yes() {
+        $class = end(explode('_', $this->field));
+        return html_writer::tag('span', 'Y', array('class' => "$class yes"));
+    }
+
     public function format($user) {
         switch ($this->field) {
-            case 'user_degree':
             case 'user_ferpa':
-                return !empty($user->{$this->field}) ? 'Y' : 'N';
+            case 'user_degree':
+                return !empty($user->{$this->field}) ? $this->span_yes() : 'N';
             case 'user_reg_status':
                 return isset($user->{$this->field}) ?
                     date('m-d-Y', $user->{$this->field}) : '';
