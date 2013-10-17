edf9dd0697cf83cb4e5b3799acf760a4ab32eeb2
Robert Russo <rrusso@lsu.edu>
2013-10-17 16:31:26 -0500

    Adds addinstance

--- blocks/course_overview/db/access.php
+++ blocks/course_overview/db/access.php
@@ -32,7 +32,18 @@ $capabilities = array(
         'archetypes' => array(
             'user' => CAP_ALLOW
         ),
-
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    'block/course_overview:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
     )
 );
