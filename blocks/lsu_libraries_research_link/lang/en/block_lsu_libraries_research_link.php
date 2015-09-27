9a7c901261ab954325dbb36a3e02accc7128b784
Robert Russo <rrusso@lsu.edu>
2015-09-26 23:25:46 -0500

    Initial add of libraries block

---
+++ blocks/lsu_libraries_research_link/lang/en/block_lsu_libraries_research_link.php
@@ -0,0 +1,6 @@
+<?php
+$string['pluginname'] = 'LSU Libraries Research Link Block';
+$string['lsu_libraries_research_link'] = 'LSU Libraries Research Link';
+$string['lsu_libraries_research_link:addinstance'] = 'Add a new LSU Libraries Research Link block';
+$string['lsu_libraries_research_link:myaddinstance'] = 'Add a new LSU Libraries Research Link block to the My Moodle page';
+$string['icon_alt'] = 'LSU Icon';
