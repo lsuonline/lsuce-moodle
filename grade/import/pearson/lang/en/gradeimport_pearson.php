e854b42edceb53b424c182cbbfed4e98158d4f63
Robert Russo <rrusso@lsu.edu>
2020-12-14 15:41:10 +0000

    Initial add of Pearson grade import plugin

---
+++ grade/import/pearson/lang/en/gradeimport_pearson.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * @package    grade_import_pearson
+ * @copyright  2008 onwards Louisiana State University
+ * @copyright  2008 onwards Robert Russo, Adam Zapletal, Philip Cali
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// General
+$string['pluginname'] = 'Pearson file';
+$string['pearson:view'] = 'View the Pearson importer';
+
+// Import Form
+$string['file'] = 'File';
+$string['upload_file'] = 'Upload File';
+$string['file_type'] = 'File Type';
+$string['my_math_lab'] = 'My Math Lab';
+$string['my_stat_lab'] = 'My Stat Lab';
+$string['mastering_chemistry'] = 'Mastering Chemistry';
+$string['mastering_biology'] = 'Mastering Biology';
+$string['mastering_physics'] = 'Mastering Physics';
+
+// Mapping Form
+$string['map_grade_items'] = 'Map Grade Items';
+$string['ignore_this_item'] = 'Ignore this item';
+
+// Mapping Page
+$string['success'] = 'Grades were successfully imported';
+$string['failure'] = 'An error has occurred';
+
+// Results Form
+$string['import_results'] = 'Import Notices';
+$string['user_not_found'] = '{$a} is not enrolled in this course and was skipped';
+
+?>
