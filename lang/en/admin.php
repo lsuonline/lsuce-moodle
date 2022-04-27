87290fdbee0dec88f50a142341f3c06cd8bdd181
Robert Russo <rrusso@lsu.edu>
2022-04-27 15:45:20 +0000

    Initial commit of LSU auto BCC code

--- lang/en/admin.php
+++ lang/en/admin.php
@@ -1481,3 +1481,10 @@ $string['managelicenses'] = 'Manage licences';
 $string['ferpaphotos'] = 'FERPA photo mode';
 $string['ferpaphotoshelp'] = 'Hides student photos from other students';
 // END LSU FERPA photos.
+
+// BEGIN LSU BCC Emails.
+$string['bccallmail'] = 'BCC all mail';
+$string['configbccallmail'] = 'BCC all mail to the specified bcc address for retention purposes.';
+$string['bccaddress'] = 'BCC address';
+$string['configbccaddress'] = 'IF BCC all mail is on, this address will receive all mail sent from this Moodle.';
+// END LSU BCC Emails.
