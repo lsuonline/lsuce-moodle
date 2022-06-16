e0b0769f083810fc6498522aea566e72af574d5f
Robert Russo <rrusso@lsu.edu>
2022-06-16 20:03:02 +0000

    Update my picture block to not upset the photo id folks

--- blocks/my_picture/lang/en/block_my_picture.php
+++ blocks/my_picture/lang/en/block_my_picture.php
@@ -41,10 +41,10 @@ $string["grab_mypictures"]   = "Grab my_picture images";
 
 // Strings for reprocess.php.
 $string['reprocess_title'] = 'Reprocess My Picture';
-$string['badiduser'] = 'You do not have a valid LSU ID Number. <br />This number will be populated when you are actively enrolled in Moodle courses. The system will automatically load your latest photo on file with the LSU Auxilary Services <a href="https://tcard.lsu.edu/imageupload.php" target="_blank">Tiger Card</a> Office.';
-$string['nopicuser'] = 'We were unable to find your photo within the LSU Auxilary Services <a href="https://tcard.lsu.edu/imageupload.php" target="_blank">Tiger Card</a> system. <br />Please visit <a href="https://tcard.lsu.edu/imageupload.php" target="_blank">Tiger Card</a> online or at 109 LSU Student Union on the LSU campus.';
+$string['badiduser'] = 'You do not have a valid LSU ID Number. <br />This number will be populated when you are actively enrolled in Moodle courses. The system will automatically load your latest photo on file with the LSU Auxilary Services Tiger Card office.';
+$string['nopicuser'] = 'We were unable to find your photo within the LSU Auxilary Services Tiger Card system. <br />If you are unhappy with your current photo or do not have a photo, please consider uploading your photo to your Moodle user profile.';
 $string['erroruser'] = 'An error has occurred';
-$string['successuser'] = 'The photo above was fetched from the LSU Auxilary Services <a href="https://tcard.lsu.edu/imageupload.php" target="_blank">Tiger Card</a> Office. <br />Please clear your browser\'s cache, log out, log back in, and refresh the dashboard if your image isn\'t updated to match the photo above.';
+$string['successuser'] = 'The photo above was fetched from the LSU Auxilary Services Tiger Card office. <br />Please clear your browser\'s cache, log out, log back in, and refresh the dashboard if your image isn\'t updated to match the photo above. <br />If you are unhappy with your current photo or do not have a photo, please consider uploading your photo to your Moodle user profile.';
 
 // Strings for fetch_missing.php and reprocess_all.php.
 $string['reprocess_all_title'] = 'Reprocess All Profile Pictures';
@@ -75,25 +75,4 @@ Reprocess Your Profile Picture
 
 Pressing Reprocess under the My Profile Picture block requests your latest photo from the Tiger Card office.
 
-You can update your Tiger Card photo by going to <a href="https://tcard.lsu.edu">https://tcard.lsu.edu</a> and submitting your own photo that adhere\'s to the Tiger Card Photo Requirements listed below
-
-1. Open a browser and go to the photos area of the Tiger Card Website: <a href="https://tcard.lsu.edu">https://tcard.lsu.edu</a>
-
-2. Here you will enter your PAWS ID and Password to login.
-
-3. Then you will be able to upload a photo to their server. This will need to be a "head shot" similar to the current photo.
-
-4. You will get an initial email when the photo is received.
-
-5. Tiger Card will do what is necessary to get the photo approved and add the photo to their server by running a server update. You will get a second email letting you know whether or not the photo is approved; however, you still need to allow additional time for the server update to run. If the photo is rejected, you will also be notified.
-
-6. The job that updates the Tiger Card server with new images runs twice a day (1 am and 2 pm, Monday-Friday), and the update takes about 30 minutes to run. So, by 1:30 am or 2:30 pm the update should be finished running each day. So even though you may get a message saying that your image is approved, it may not be immediately available to reprocess in Moodle. You will need to wait for the next update time to pass.
-
-7. Once the photo has been updated, you log into Moodle.
-
-8. On your My Courses page, Turn Editing On (upper right) and look for the Add Blocks block to appear. Use this block to add the My Profile Picture block to the page.
-
-9. Click on the Reprocess link in this block and this will replace the former picture with your new picture.
-
-10. To see your image, clear your browser\'s cache, log out and back in, and refresh the Moodle page.
-';
+You can update your Moodle photo via your Moodle user profile.';
