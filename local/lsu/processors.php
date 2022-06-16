99d9fae82cbaea42f47f1990e48e83ddc6a08405
Robert Russo <rrusso@lsu.edu>
2022-06-16 14:51:50 +0000

    Hardcode new date for sports seasons in UES Enrollment Providers.

--- local/lsu/processors.php
+++ local/lsu/processors.php
@@ -382,7 +382,7 @@ class lsu_sports extends lsu_source {
     function find_season($time) {
         $now = getdate($time);
 
-        $june = 604;
+        $june = 521;
         $dec = 1231;
 
         $cur = (int)(sprintf("%d%02d", $now['mon'], $now['mday']));
