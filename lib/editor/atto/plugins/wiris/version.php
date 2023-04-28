1bef54c4dbd4f11bae1d661696bee6cdc414af6b
Robert Russo <rrusso@lsu.edu>
2023-04-28 15:02:24 +0000

    Updating wiris to latest.

--- lib/editor/atto/plugins/wiris/version.php
+++ lib/editor/atto/plugins/wiris/version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022112300;
-$plugin->release = '8.0.1';
+$plugin->version = 2023042400;
+$plugin->release = '8.2.6';
 $plugin->requires = 2014050800;
 $plugin->component = 'atto_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2022112300);
+$plugin->dependencies = array ('filter_wiris' => 2023042400);
 $plugin->maturity = MATURITY_STABLE;
