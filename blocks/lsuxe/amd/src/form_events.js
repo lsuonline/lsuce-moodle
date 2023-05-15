3032b437cae273cea7d17735710d5a4232b85f56
David <dlowe6@lsu.edu>
2023-05-15 16:16:04 -0600

    Feature - New Block Testing - Lang fix for LSUXE

--- blocks/lsuxe/amd/src/form_events.js
+++ blocks/lsuxe/amd/src/form_events.js
@@ -274,12 +274,11 @@
                         that.setHiddenValue('srccourseid', response.data.id);
                         that.setHiddenValue('srccoursegroupid', response.data.groupid);
                         Noti.callNoti({
-                            message: "Everything checks out for the sourse course and group.",
+                            message: "Everything checks out for the source course and group.",
                             type: 'success'
                         });
                     }
                 });
-
             });
 
             $('#id_verifydest').on('click', function() {
