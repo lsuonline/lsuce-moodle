9b6c0dd1aa833938f94fa304520a8aee69bc5788
Robert Russo <rrusso@lsu.edu>
2023-08-17 15:58:20 +0000

    Update wiris plugins to match LSU

--- lib/editor/tinymce/plugins/tiny_mce_wiris/version.php
+++ lib/editor/tinymce/plugins/tiny_mce_wiris/version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022070100;
-$plugin->release = '7.30.0';
+$plugin->version = 2022112300;
+$plugin->release = '8.0.1';
 $plugin->requires = 2012120300;
 $plugin->component = 'tinymce_tiny_mce_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2022070100);
+$plugin->dependencies = array ('filter_wiris' => 2022112300);
 $plugin->maturity = MATURITY_STABLE;
