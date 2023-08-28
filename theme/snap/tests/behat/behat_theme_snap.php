3830a1a946173c7731dcea9182ad62aa0d4cb71d
David <dlowe6@lsu.edu>
2023-08-28 17:01:10 -0600

    feature - Snap update for M41

--- theme/snap/tests/behat/behat_theme_snap.php
+++ theme/snap/tests/behat/behat_theme_snap.php
@@ -259,7 +259,7 @@ class behat_theme_snap extends behat_base {
         $fs->create_file_from_pathname($fileinfo, $CFG->dirroot . "/theme/snap/tests/fixtures/testpng.png");
 
         $this->i_am_on_course_page($shortname);
-        $this->execute('behat_general::click_link', ['Create a new section']);
+        $this->execute('behat_general::i_click_on', ['#snap-new-section', 'css_element']);
         $this->execute('behat_general::i_click_on', ['Insert or edit image', 'button']);
         $this->execute('behat_general::i_click_on', ['Browse repositories...', 'button']);
         $this->execute('behat_general::i_click_on', ['Private files', 'link', '.fp-repo-area', 'css_element']);
@@ -517,7 +517,7 @@ class behat_theme_snap extends behat_base {
     public function i_restrict_course_section_by_date($section, $date) {
         $datetime = strtotime($date);
         $this->i_go_to_course_section($section);
-        $this->click_visible_link('Edit section');
+        $this->execute('behat_general::i_click_on', ['#section-'.$section.' .edit-summary', 'css_element']);
         $this->i_wait_until_is_visible('.editor_atto', 'css_element');
         $this->execute('behat_forms::i_set_the_field_to', ['name[customize]', '1']);
         $this->execute('behat_forms::i_set_the_field_to', ['name[value]', 'Topic '.$date.' '.$section]);
