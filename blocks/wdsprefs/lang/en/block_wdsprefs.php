a650276dea57f81c2b1eedf6b557c7c12e0ef1e6
Robert Russo <rrusso@lsu.edu>
2025-03-26 19:43:03 +0000

    Initial add of WDS preferences

---
+++ blocks/wdsprefs/lang/en/block_wdsprefs.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    block_wdsprefs
+ * @copyright  2025 onwards Louisiana State University
+ * @copyright  2025 onwards Robert Russo
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Plugin stuff.
+$string['wdsprefs:pluginname'] = 'WDS Preferences';
+
+// Basic stuff.
+$string['wdsprefs:cancel'] = 'Preference modification cancelled.';
+$string['wdsprefs:error'] = 'Error saving preferences!';
+$string['wdsprefs:saveprefs'] = 'Save Preferences';
+$string['wdsprefs:success'] = 'Preferences saved successfully!';
+
+// Link stuff.
+$string['wdsprefs:course'] = 'Course preferences';
+$string['wdsprefs:crosslist'] = 'Cross listing';
+$string['wdsprefs:split'] = 'Split courses';
+$string['wdsprefs:schedule'] = 'Course schedule';
+$string['wdsprefs:teamteach'] = 'Team teaching';
+$string['wdsprefs:unwant'] = 'Unwanted courses';
+$string['wdsprefs:user'] = 'User preferences';
+
+// Page strings.
+$string['wdsprefs:daysprior'] = 'Days Prior';
+$string['wdsprefs:daysprior_desc'] = 'Number of days prior to the semester starting to enroll students.';
