956ab5f604fc5a076018450f9fd4254a42b1c173
David <dlowe6@lsu.edu>
2025-06-05 11:29:33 -0600

    hotfix - Moodle 4.5.3 Quickmail send all option for students

--- blocks/quickmail/classes/forms/compose_message_form.php
+++ blocks/quickmail/classes/forms/compose_message_form.php
@@ -27,7 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->libdir . '/formslib.php');
 
-
 use block_quickmail\forms\concerns\is_quickmail_form;
 use block_quickmail_plugin;
 use block_quickmail_string;
@@ -744,19 +743,26 @@ class compose_message_form extends \moodleform {
 
     private function get_recipient_entities() {
         $results = [];
-
-        
-        $roleids = role_repo::get_user_roles_in_course($this->user->id, $this->course->id);
         $found = false;
-        foreach ($roleids as $dis_id) {
-            if ($dis_id == 5) {
-                // user is a student, no 'All in course' option for them.
-                $found = true;
-            }
-        }
 
-        if ($found == false) {
+        $can_send = get_config('moodle', 'block_quickmail_misc_allow_student_sendall');
+        // If the setting is on then send to all in course regardless.
+        if ($can_send == "1") {
             $results['all'] = block_quickmail_string::get('all_in_course');
+        } else {
+            // Setting is off so only teachers can send to all.
+            $roleids = role_repo::get_user_roles_in_course($this->user->id, $this->course->id);
+
+            foreach ($roleids as $dis_id) {
+                if ($dis_id == 5) {
+                    // user is a student, no 'All in course' option for them.
+                    $found = true;
+                }
+            }
+
+            if ($found == false) {
+                $results['all'] = block_quickmail_string::get('all_in_course');
+            }
         }
 
         foreach (['role', 'group', 'user'] as $type) {
