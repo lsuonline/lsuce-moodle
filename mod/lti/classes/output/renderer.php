2adfe07bcfe630d9a5f093e639136f9b5231f6a9
David <dlowe6@lsu.edu>
2025-08-21 07:51:52 -0600

    feature - Removed LTI changes and changed the name of "Add tool" to "Add new tool"

--- mod/lti/classes/output/renderer.php
+++ mod/lti/classes/output/renderer.php
@@ -96,10 +96,8 @@ class renderer extends plugin_renderer_base {
         $headerrenderable = $page->get_header();
         $table = $page->get_table();
         $headercontext = $headerrenderable->export_for_template($this);
-        
         $headeroutput = parent::render_from_template('mod_lti/course_tools_page_header', $headercontext);
-        $footeroutput = parent::render_from_template('mod_lti/course_tools_page_footer', $headercontext);
 
-        return $headeroutput . $table->output(). $footeroutput;
+        return $headeroutput . $table->output();
     }
 }
